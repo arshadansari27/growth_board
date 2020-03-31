de4402084563a279efc2f5a73510a94b55258f78
Arshad Ansari <arshadansari27@gmail.com>
2020-04-01 00:54:05 +0530

    Requirements updated

--- daemon.py
+++ daemon.py
@@ -12,6 +12,12 @@ def hotspot_updater():
     print('This job is run every hour.')
     update_daily_hotspots()
 
+
+@sched.scheduled_job('interval', minutes=1)
+def log_updater():
+    print('This job is runs every minute to let you know that things are '
+          'actually running.')
+
 @sched.scheduled_job('cron', day_of_week="mon-fri", hour=3)
 def task_updater():
     update_tasks()
