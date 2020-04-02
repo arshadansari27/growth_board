709c6e4d079c0f3be3b9288edcf54c8415192643
Arshad Ansari <arshadansari27@gmail.com>
2020-04-02 17:09:34 +0530

    Daemon timings should mismatch

--- daemon.py
+++ daemon.py
@@ -4,22 +4,25 @@ from notion_api.time_agg_updater import update_all_time_aggregates
 from notion_api.hotspot_updater import update_daily_hotspots
 from notion_api.task_updater import update_tasks
 
+
 sched = BlockingScheduler()
 
-@sched.scheduled_job('interval', hours=1)
+
+@sched.scheduled_job('interval', minutes=25)
 def hotspot_updater():
-    print('This job is run every hour.')
     update_daily_hotspots()
     update_all_time_aggregates()
 
 
-@sched.scheduled_job('interval', minutes=5)
+@sched.scheduled_job('interval', minutes=7)
 def log_updater():
     print('This job is runs every minute to let you know that things are '
           'actually running.')
 
+
 @sched.scheduled_job('interval', hours=4)
 def task_updater():
     update_tasks()
+
 
 sched.start()
