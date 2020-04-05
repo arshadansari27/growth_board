eeb154efd990db11bb793d0d72e305a5cb146e30
Arshad Ansari <arshadansari27@gmail.com>
2020-04-05 12:41:19 +0530

    Fixes

--- daemon.py
+++ daemon.py
@@ -1,6 +1,6 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 
-from notion_api.calendar_updater import calendar_events
+from notion_api.calendar_updater import update_calendar_times
 from notion_api.time_agg_updater import update_all_time_aggregates
 from notion_api.hotspot_updater import update_daily_hotspots
 from notion_api.task_updater import update_tasks
@@ -9,7 +9,7 @@ from notion_api.task_updater import update_tasks
 sched = BlockingScheduler()
 
 
-@sched.scheduled_job('interval', minutes=25)
+@sched.scheduled_job('interval', minutes=23)
 def hotspot_updater():
     print('Runs every 25 mins for daily updates')
     update_daily_hotspots()
@@ -19,7 +19,7 @@ def hotspot_updater():
 @sched.scheduled_job('interval', minutes=4)
 def log_updater():
     print('Runs every 4 mins for calendar update')
-    calendar_events()
+    update_calendar_times()
 
 
 @sched.scheduled_job('interval', minutes=31)
