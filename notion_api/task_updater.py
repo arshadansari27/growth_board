33af714aef051bff59dd45eee9c6d07a0ce7486f
Arshad Ansari <arshadansari27@gmail.com>
2020-04-08 15:02:22 +0530

    Updates

--- notion_api/task_updater.py
+++ notion_api/task_updater.py
@@ -34,8 +34,10 @@ def update_from_hiring_board():
         task_row.project = hiring_project.id
         task_row.task_type = 'Story'
         task_row.link = _task.get_browseable_url()
-        if getattr(_task, 'Schedule', None) and isinstance(_task.Schedule, NotionDate):
-            task_row.scheduled = NotionDate(_task.Schedule.start, _task.Schedule.end, DEFAULT_TIMEZONE)
+        if getattr(_task, 'Scheduled', None) and isinstance(_task.Scheduled,
+                                                            NotionDate):
+            task_row.scheduled = NotionDate(_task.Scheduled.start,
+                                            _task.Scheduled.end, DEFAULT_TIMEZONE)
         if task.Status in {'Dropped', 'Offered'}:
             task_row.done = True
         else:
